293ca1e0ce688ec43742f20f2a984dc7ed16f1a0
pandusonu2 <pandu.sonu@yahoo.com>
2015-11-14 19:51:40 +0530

    Input and break

--- downloader.sh
+++ downloader.sh
@@ -1,25 +1,28 @@
-manga='the-gamer'
-chap='107'
+echo "Enter Manga name:"
+read manga
+echo "Enter the chapter number:"
+read chap
 mkdir $manga
 cd $manga
-for i in {1..21}
+for i in {19..25}
 do
 	echo "Downloading $i page....."
-	wget -o log.txt -O $i.html www.mangareader.net/$manga/$chap/$i
-	#grep src=\"http://i999.mangareader.net/the-gamer/ $i.html > jump.txt
+	wget -o log.txt -O $i.html -c www.mangareader.net/$manga/$chap/$i
 	grep '<div id=\"imgholder\"><a href=' $i.html > jump.txt
 	link=$(head -n 1 jump.txt)
 	starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "11q;d")"
 	endi="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "12q;d")"
-	#$link | grep -aob '"' | grep -oE '[0-9]+' | sed "12q;d"
-	#starti=$(expr index "$link" \")
 	length=$((endi-starti))
 	image=${link:$((starti+1)):$((length-1))}
+	length=${#image}
+	if [[ "$length" -eq 0 ]]; then
+		break
+	fi
 	wget -O $i.jpg -o log.txt -c $image
-	rm $i.html
 done
 echo "Converting to pdf..."
 convert *.jpg chap.pdf
 echo "Cleaning up....."
+rm *.html
 rm jump.txt
 rm log.txt
