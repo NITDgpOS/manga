7c332fc8defecd3978f886cc9ad6dbef68bd0da5
pandusonu2 <pandu.sonu@yahoo.com>
2015-11-16 21:40:01 +0530

    Corrected error with Large Images

--- downloader.sh
+++ downloader.sh
@@ -4,14 +4,18 @@ echo "Enter the chapter number:"
 read chap
 mkdir $manga
 cd $manga
-for i in {19..25}
+for i in {1..25}
 do
 	echo "Downloading $i page....."
 	wget -o log.txt -O $i.html -c www.mangareader.net/$manga/$chap/$i
-	grep '<div id=\"imgholder\"><a href=' $i.html > jump.txt
+	grep 'src=\"http' $i.html | grep 'mangareader' > jump.txt
 	link=$(head -n 1 jump.txt)
 	starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "11q;d")"
 	endi="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "12q;d")"
+	if grep -q "Larger Image" $i.html; then
+		starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "9q;d")"
+		endi="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "10q;d")"
+	fi
 	length=$((endi-starti))
 	image=${link:$((starti+1)):$((length-1))}
 	length=${#image}
