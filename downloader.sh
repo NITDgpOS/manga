bedd0eddd7e5c9d58a2c84c7b06f9d92ad8736ec
pandusonu2 <pandu.sonu@yahoo.com>
2015-11-17 22:25:13 +0530

    Major changes #2

--- downloader.sh
+++ downloader.sh
@@ -1,32 +1,44 @@
 echo "Enter Manga name:"
 read manga
-echo "Enter the chapter number:"
-read chap
+echo "Enter the chapter range."
+echo "Start:"
+read chaps
+echo "End:"
+read chape
+echo $manga >> .gitignore
+sort -u -o .gitignore .gitignore
 mkdir $manga
 cd $manga
-for i in {1..25}
+for chap in $(seq $chaps $chape);
 do
-	echo "Downloading $i page....."
-	wget -o log.txt -O $i.html -c www.mangareader.net/$manga/$chap/$i
-	grep 'src=\"http' $i.html | grep 'mangareader' > jump.txt
-	link=$(head -n 1 jump.txt)
-	starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "11q;d")"
-	endi="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "12q;d")"
-	if grep -q "Larger Image" $i.html; then
-		starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "9q;d")"
-		endi="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "10q;d")"
-	fi
-	length=$((endi-starti))
-	image=${link:$((starti+1)):$((length-1))}
-	length=${#image}
-	if [[ "$length" -eq 0 ]]; then
-		break
-	fi
-	wget -O $i.jpg -o log.txt -c $image
+	mkdir $chap
+	cd $chap
+	for i in {1..100}
+	do
+		echo "Downloading page $i of chapter $chap....."
+		wget -o log.txt -O $i.html -c www.mangareader.net/$manga/$chap/$i
+		grep 'src=\"http' $i.html | grep 'mangareader' > jump.txt
+		link=$(head -n 1 jump.txt)
+		starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "11q;d")"
+		endi="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "12q;d")"
+		if grep -q "Larger Image" $i.html; then
+			starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "9q;d")"
+			endi="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "10q;d")"
+		fi
+		length=$((endi-starti))
+		image=${link:$((starti+1)):$((length-1))}
+		length=${#image}
+		if [[ "$length" -eq 0 ]]; then
+			break
+		fi
+		imagename=0000$i
+		wget -O ${imagename: -4}.jpg -o log.txt -c $image
+	done
+	echo "Converting to pdf..."
+	chapno=0000$chap
+	chapno=${chapno: -4}
+	convert *.jpg ../chap$chapno.pdf
+	echo "Cleaning up....."
+	cd ..
+	rm -rf $chap
 done
-echo "Converting to pdf..."
-convert *.jpg chap.pdf
-echo "Cleaning up....."
-rm *.html
-rm jump.txt
-rm log.txt
