36802e25d98942942b5219fc99f6804f31529aae
pandusonu2 <pandu.sonu@yahoo.com>
2015-11-20 23:16:04 +0530

    Working newsteller

--- newsteller.sh
+++ newsteller.sh
@@ -1,2 +1,9 @@
-wget -O index.html www.mangareader.net
-grep "chaptersrec" 
+wget -o jump.txt  -O index.html www.mangareader.net
+grep 'chaptersrec' index.html > jump.txt
+while read p;do
+  starti="$(echo $p | grep -aob '>' | grep -oE '[0-9]+' | sed "1q;d")"
+  endi="$(echo $p | grep -aob '<' | grep -oE '[0-9]+' | sed "2q;d")"
+  length=$((endi-starti))
+  newchap=${p:$((starti+1)):$((length-1))}
+  echo $newchap
+done < jump.txt
