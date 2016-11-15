634ba87399765e14a2df11e1b46f66442b088874
Indraneel <indraneel.sarkar96@gmail.com>
2016-11-15 23:42:01 +0530

    Added changes to ensure flexibility

--- page.sh
+++ page.sh
@@ -2,6 +2,16 @@
 
 wget -O index.html www.mangareader.net
 
-#Open the index page on Google Chrome
-google-chrome-stable index.html
-
+#Open the index page on Firefox or Chrome as per user choice
+echo -e "Open using\n(1)Chrome\n(2)Firefox\n"
+echo -e "Enter choice:(1/2)"
+read num
+if [ $num == 1 ]
+	then 	
+		google-chrome-stable index.html
+elif [ $num == 2 ]
+	then 
+		firefox index.html
+else
+	echo "invalid choice"
+fi
