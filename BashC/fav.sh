e15023f3dc2cd829f486141f860edd5b19974586
souravagarwal5377 <souravagarwal5377@gmail.com>
2018-01-29 02:29:21 +0530

    Fav.sh corrected

--- BashC/fav.sh
+++ BashC/fav.sh
@@ -1,8 +1,8 @@
 check () {
-  mangaName = $1
-  mangaName = ${m,,} # Converting it to lower case
-  mangaName = ${m// /-} # Removing spaces and adding - in their places
-  if curl -s --head http://www.mangareader.net/$mangaName | grep "200 OK" > /dev/null
+  mangaName=$1
+  mangaName=${m,,} # Converting it to lower case
+  mangaName=${m// /-} # Removing spaces and adding - in their places
+  if curl -s --head  http://www.mangareader.net/$mangaName | grep "200 OK" > /dev/null
   then
     echo $mangaName >> .fav
     echo "Manga added to fav list"
